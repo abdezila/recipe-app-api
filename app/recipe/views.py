69fd5b8b340fbc2fabe1b96783df2d7fba681846
Abdou Nemmour <abderahmenenemmour@gmail.com>
2025-12-05 18:04:28 +0100

    test and implemetn of delete tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -34,7 +34,8 @@ class RcipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.UpdateModelMixin,
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
                  mixins.ListModelMixin,
                  viewsets.GenericViewSet):
     """Manage tags in the database."""
