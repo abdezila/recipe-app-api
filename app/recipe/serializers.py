ad774e86f716e694657474037f75a8b56b020933
Abdou Nemmour <abderahmenenemmour@gmail.com>
2025-12-05 16:46:39 +0100

    implemention of list tag

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,9 @@
 """Serializers for recipe APIs"""
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
@@ -14,3 +17,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serailizer for recipe detail view."""
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+        
