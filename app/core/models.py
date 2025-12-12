3702e0c78424790c05b94e9f1584f39f19b7a624
Abdou Nemmour <abderahmenenemmour@gmail.com>
2025-12-12 09:13:04 +0100

    add test and implement for deleting ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -26,7 +26,6 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-
 class User(AbstractBaseUser,PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255,unique=True)
