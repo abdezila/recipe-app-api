432e303568e83789ec7d8fd5bce70edc3ee93fce
Abdou Nemmour <abderahmenenemmour@gmail.com>
2025-12-24 20:51:28 +0100

    so change

--- app/paper/permissions.py
+++ app/paper/permissions.py
@@ -22,17 +22,17 @@ class PaperPermissions(BasePermission):
         if request.method in SAFE_METHODS:
             return True
 
-        # create paper
+
         if request.method == "POST":
             return is_staff(request.user) or is_author(request.user)
 
-        # update/delete paper
+
         return is_staff(request.user)
 
     def has_object_permission(self, request, view, obj):
-        # read-only is allowed
+
         if request.method in SAFE_METHODS:
             return True
 
-        # only staff can modify any paper (accept/reject/update/delete)
+
         return is_staff(request.user)
