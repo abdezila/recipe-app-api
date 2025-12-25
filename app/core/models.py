576015f994f3c002f5862204a8201be0e14f1ac1
Abdou Nemmour <abderahmenenemmour@gmail.com>
2025-12-25 15:11:07 +0100

    alter feild of role user, create permissions for event registration

--- app/core/models.py
+++ app/core/models.py
@@ -42,8 +42,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
-        null = True,
-        blank=True
+        default=Role.PARTICIPANT,
     )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
---
+++ app/event/permissions.py
@@ -0,0 +1,13 @@
+"""Permissions of Event objects and sub objects."""
+from rest_framework.permissions import (BasePermission,
+                                        SAFE_METHODS,)
+from core.models import EventRegistration
+
+class CanRegisterToEvent(BasePermission):
+    """Allows author and participant register to Event Registration."""
+    def has_permission(self, request, view):
+        user = request.user
+        if not user or user.is_authenticated:
+            return False
+        
+        return user.role in (user.Role.AUTHOR or user.Role.PARTICIPANT)
